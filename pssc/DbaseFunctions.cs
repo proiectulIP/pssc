d4087b90e2d3b03aca4bcb66f57641707b05ec9d
proiectulIP <thebat56@yahoo.com>
2015-11-07 22:24:49 +0200

    comm

---
+++ pssc/DbaseFunctions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+
+namespace pssc
+{
+    class DbaseFunctions
+    {
+        private string connectionString;
+        private string userType;
+        private OleDbConnection connection = new OleDbConnection();
+
+        public DbaseFunctions(string dbasePath,string browseType)
+        {
+            connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;" +
+        @"Data source= "+ dbasePath;
+            userType = browseType;
+
+            connection.ConnectionString = connectionString;
+        }
+
+        public void dbaseConnect()
+        {
+            try
+            {
+                connection.Open();
+            }
+
+            catch(OleDbException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void checkLoginCredintials(string user, string pass)
+        {
+            OleDbCommand command = new OleDbCommand("select * from users", connection);
+            OleDbDataReader reader;
+            reader = command.ExecuteReader();
+
+            while(reader.Read())
+            {
+                string username = reader["username"].ToString();
+                string password = reader["pass"].ToString() ;
+
+                if((user.Equals(username)) && (pass.Equals(password)))
+                    {
+                    MessageBox.Show("success");
+                    break;
+                }
+
+            }
+        }
+    }
+}
---
+++ pssc/loginForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pssc
+{
+    public class loginForm:Form
+    {
+        private RadioButton profRadioButton;
+        private RadioButton studRadioButton;
+        private RadioButton cazButton;
+
+
+        public string radioButtonChecked()
+        {
+            int btn = 0;
+
+            if (ProfRadioButton.Checked)
+                btn = 1;
+            else if (StudRadioButton.Checked)
+                btn = 2;
+            else if (CazButton.Checked)
+                btn = 3;
+
+            var fret = mapButtonToType(btn);
+            return fret;
+        }
+
+        private string mapButtonToType(int btn)
+        {
+            string fret = "aaa";
+            switch (btn)
+            {
+                case 1: { fret = "profesor"; break; }
+                case 2: { fret = "student"; break; }
+                case 3: { fret = "cazare"; break; }
+            }
+
+            return fret;
+        }
+
+        public RadioButton ProfRadioButton
+        {
+            get
+            {
+                return profRadioButton;
+            }
+
+            set
+            {
+                profRadioButton = value;
+            }
+        }
+
+        public RadioButton StudRadioButton
+        {
+            get
+            {
+                return studRadioButton;
+            }
+
+            set
+            {
+                studRadioButton = value;
+            }
+        }
+
+        public RadioButton CazButton
+        {
+            get
+            {
+                return cazButton;
+            }
+
+            set
+            {
+                cazButton = value;
+            }
+        }
+
+
+    }
+}
